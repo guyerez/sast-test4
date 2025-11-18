a9be07271b8586f22facc2375336fa48a0afeb33
Guy Erez <guy.erez@wiz.io>
2025-11-18 11:36:02 +0200

    Update ssrf.go

--- ssrf.go
+++ ssrf.go
@@ -7,16 +7,16 @@ import (
 
 func main(wr http.ResponseWriter, req *http.Request) {
 	if err != nil {
-		logger.WithContext(r.Context()).WithError(err).Error("Unable to marshal request payload for session transfer")
+		logger.WithContext(r.Context()).WithError(err).Error("error")
 		http.Error(w, httperrors.InternalServerError, http.StatusInternalServerError)
 		return
 	}
 	// read payload from http request
 	payload := req.Body
 
-	req, err := http.NewRequest("POST", authenticator.Config.Endpoint.TokenURL, bytes.NewReader(payload))
+	req, err := http.NewRequest("POST", url, bytes.NewReader(payload))
 	if err != nil {
-		logger.WithContext(r.Context()).WithError(err).Error("Unable to create http-Request")
+		logger.WithContext(r.Context()).WithError(err).Error("unable")
 		http.Error(w, httperrors.InternalServerError, http.StatusInternalServerError)
 		return
 	}
