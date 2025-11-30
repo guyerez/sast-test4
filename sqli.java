726c94d8a0834349248d623e30281f79cc49cb3b
Guy Erez <guy.erez@wiz.io>
2025-11-30 15:37:49 +0200

    Update sqli.java

--- sqli.java
+++ sqli.java
@@ -4,6 +4,7 @@ import java.sql.DriverManager;
 public List<User> searchUsers(String username, String order) {
     var users = new ArrayList<User>();
     var searchUser = "SELECT name FROM users WHERE name  LIKE CONCAT( '%',?,'%') ORDER BY " + order;
+    Connection connection = DriverManager.getConnection("zzzz");
     try (var ps = connection.prepareStatement( searchUser )) {
       ps.setString(1, username );
     }
