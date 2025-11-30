7bbab3900cb59002aedc0685d4774b321b88a1d6
Guy Erez <guy.erez@wiz.io>
2025-11-30 14:42:19 +0200

    Create sqli.java

---
+++ sqli.java
@@ -0,0 +1,17 @@
+import java.sql.DriverManager;
+
+
+public List<User> searchUsers(String username, String order) {
+    var users = new ArrayList<User>();
+    var searchUser = "SELECT name FROM users WHERE name  LIKE CONCAT( '%',?,'%') ORDER BY " + order;
+    try (var ps = connection.prepareStatement( searchUser )) {
+      ps.setString(1, username );
+    }
+
+    /// Just empty spaced
+    /// Just empty spaced
+    /// Just empty spaced
+    /// Just empty spaced
+    /// Just empty spaced
+  
+ }
