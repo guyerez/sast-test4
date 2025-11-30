be09989d5685fb3c0e99f7f49054cb4bd39319b7
Guy Erez <guy.erez@wiz.io>
2025-11-30 15:25:07 +0200

    Create timing.js

---
+++ timing.js
@@ -0,0 +1,37 @@
+app.get(basePath + '/admin', (req, res) => {
+  const token = req.query.token;
+  
+  if (token === 'BestAdmin123') {
+    res.send(`
+      <html>
+        <head><title>admin</title></head>
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+      </html>
+    `);
+  } else {
+    res.status(401).send(`
+      <html>
+        <head><title>admin</title></head>
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+        blah
+      </html>
+    `);
+  }
+});
