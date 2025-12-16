6b9e122709cf0e3db65f309eecfe6bd4466dabd4
Guy Erez <guy.erez@wiz.io>
2025-12-16 09:32:43 +0200

    Update sqli4.kt

--- tests/sqli4.kt
+++ tests/sqli4.kt
@@ -1,5 +1,5 @@
 fun getUserByName(userInput: String) {
     val query = "SELECT * FROM users WHERE name = '$userInput'"
-    val statement1 = connection.createStatement()
-    val result = statement1.executeQuery(query)
+    val statement2 = connection.createStatement()
+    val result = statement2.executeQuery(query)
 }
