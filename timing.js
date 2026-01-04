caeeb864bff68b8aa3e2ad3aefdb3d5ac76063e2
Guy Erez <guy.erez@wiz.io>
2026-01-04 12:01:28 +0200

    Update timing.js

--- timing.js
+++ timing.js
@@ -22,6 +22,59 @@ const number = prompt('Enter the number: ');
 
 const result = Math.sqrt(number);
 console.log(`The square root of ${number} is ${result}`);
+  // take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);// take the input from the user
+const number = prompt('Enter the number: ');
+
+const result = Math.sqrt(number);
+console.log(`The square root of ${number} is ${result}`);
   if (token === 'BestAdmin123') {
     res.send(`
       <html>
