05a8fc988259dc342d15cee36c9f6442360c61a9
Guy Erez <guy.erez@wiz.io>
2026-01-19 16:55:58 +0200

    fixed issue

--- sqli.kt
+++ sqli.kt
@@ -1,5 +1,6 @@
 fun getUserByName(userInput: String) {
-    val query = "SELECT * FROM users WHERE name = '$userInput'"
-    val statement = connection.createStatement()
-    val result = statement.executeQuery(query)
+    val query = "SELECT * FROM users WHERE name=?"
+    val statement = connection.prepareStatement(query)
+    statement.setString(1, userInput)
+    val result = statement.executeQuery()
 }
