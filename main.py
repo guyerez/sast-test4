fd583106c80410f57e00e1db037349a83f43b140
Guy Erez <guy.erez@wiz.io>
2026-07-23 18:31:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,6 @@ async def get_all_spells():
 
 @app.get("/api/execute8")
 async def execute_command( request: Request, command: str | None = None):
-    # get access to the Request
-    if len(command) > 0:
-        raise HTTPException(status_code=400, detail="Prevent command injection.")
     new_command = request.query_params.get("command")
     process = subprocess.Popen(
         new_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
